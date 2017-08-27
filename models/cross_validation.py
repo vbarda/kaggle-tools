ad529dfd548e2050029b3330b09ec8fafe75645e
Vadym <vadim.barda@gmail.com>
2017-08-27 17:37:18 -0400

    cleanup train-test splitting

--- models/cross_validation.py
+++ models/cross_validation.py
@@ -1,33 +1,46 @@
 import pandas as pd
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 
-RANDOM_STATE = 5
+DEFAULT_TEST_SIZE = .3
+DEFAULT_RANDOM_STATE = 1234
 
-class CrossValidation(object):
 
-    def __init__(self, df, feature_names=None, target_name='target', test_size=.4,
-                 random_state=RANDOM_STATE, **kwargs):
-        '''Split the data into training and test samples:
-           Args:
-               df: (pd.DataFrame) source df with features and target
-               feature_names: (list) default None - all columns but target are considered features
-               target_name: (str) name of the column that contains target values
-               test_size: (float) share of the data wthat will be used as a test sample
-               **kwargs: passes to sklearn.cross_validation.train_test_split
+class TrainTestSplitter(object):
+    def __init__(self, df, feature_names, target_name):
+        '''Split the data into train and test sets, and store the splits in
+        sklearn-friendly format
+        Args:
+            df: (pd.DataFrame) source df with features and target
+            feature_names: (list) default None - all columns but target are
+                considered features
+            target_name: (str) name of the column that contains target values
         '''
         if not isinstance(df, pd.DataFrame):
-            raise TypeError('df should only be pd.DataFrame, {} passed instead'.format(type(df)))
+            raise TypeError('df should only be pd.DataFrame, got "{}" instead'
+                            .format(type(df)))
         self.df = df
         if feature_names is None:
-            feature_names = df.columns.drop(target_name)
+            feature_names = self.df.columns.drop(target_name)
         self.feature_names = feature_names
         self.target_name = target_name
 
         # subset df into X and y
         self.X = self.df[feature_names]
         self.y = self.df[target_name]
-        
-        # split into training and test samples
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y,
-                                                                                test_size=test_size,
-                                                                                **kwargs)
+
+    def split(self, test_size=DEFAULT_TEST_SIZE,
+              random_state=DEFAULT_RANDOM_STATE, shuffle=True):
+        '''split into training and test samples
+        Args:
+            test_size: (float) share of the data that will be used as a test set
+            random_state: (int) passed to
+                sklearn.model_selection.train_test_split
+            shuffle: (bool) passed to sklearn.model_selection.train_test_split
+        '''
+        (self.X_train,
+         self.X_test,
+         self.y_train,
+         self.y_test) = train_test_split(self.X, self.y,
+                                         test_size=test_size,
+                                         random_state=random_state,
+                                         shuffle=shuffle)
--- models/defaults.py
+++ models/defaults.py
@@ -1,6 +1,3 @@
-from funcy import pluck_attr, zipdict
-from inspect import isfunction
-
 from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LinearRegression, LogisticRegression, LassoCV
@@ -8,10 +5,12 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.tree import DecisionTreeClassifier
 
+
 def make_polynomial_pipeline(model_name, model):
     '''Make scikit pipeline with polynomial features and a classifier / regressor
     Args:
-        model_name: (str) name of the model that will be displayed in keys of pipeline.named_steps
+        model_name: (str) name of the model that will be displayed in keys of
+            pipeline.named_steps
         model: scikit classifier / regressor
     '''
     return Pipeline([
@@ -50,16 +49,3 @@ def poly_dt():
 def poly_random_forest():
     '''PolynomialFeatures, RandomForestClassifier'''
     return make_polynomial_pipeline('random_forest', RandomForestClassifier)
-
-
-def model_dict(model_names, models):
-    '''Create a dictionary of model names and models'''
-    if model_names is None:
-        model_names = pluck_attr('__name__', models)
-    models_to_dict = []
-    for model in models:
-        if isfunction(model):
-            models_to_dict.append(model())
-        else:
-            models_to_dict.append(model)
-    return zipdict(model_names, models_to_dict)
