c167fa14eb8bc1fa3d441a9ebe157775e670b6f3
Vadym <vadim.barda@gmail.com>
2018-04-02 19:52:13 -0400

    make parallel_groupby_apply Python 2.x compatible and add group names

--- utils/misc.py
+++ utils/misc.py
@@ -1,13 +1,30 @@
+from contextlib import closing
 from multiprocessing import Pool, cpu_count
 
 import pandas as pd
 
+from utils.validation import validate_iterable
 
-def parallel_apply(gb, func):
-    '''Parallelize groupby'''
-    with Pool(cpu_count()) as p:
-        ret_list = p.map(func, [grp_df for grp_name, grp_df in gb])
-    return pd.concat(ret_list)
+
+def parallel_groupby_apply(df, groupby, func, n_cpus=None):
+    '''Groupby and apply using multiple cores
+
+    Args:
+        df: (pd.DataFrame)
+        groupby: (list) of columns to group by
+        func: (func) that will be applied to each group
+        n_cpus: (int) how many CPUs to use. Defaults to maximum available
+    '''
+    validate_iterable(groupby)
+    g = df.groupby(groupby)
+    n_cpus = n_cpus or cpu_count()
+    keys = [grp_name for grp_name, _ in g]
+    with closing(Pool(n_cpus)) as p:
+        processed_dfs = p.map(func, [grp_df for _, grp_df in g])
+    concatted = pd.concat(processed_dfs, keys=keys)
+    existing_idx_names = list(concatted.index.names)
+    idx_names = groupby + existing_idx_names[len(groupby):]
+    return concatted.rename_axis(idx_names)
 
 
 def add_next_periods_padding(df, n_periods=1, freq=None):
@@ -19,9 +36,11 @@ def add_next_periods_padding(df, n_periods=1, freq=None):
         freq: (str) if None, will try to infer from existing datetime index
     '''
     if not isinstance(df.index, pd.DatetimeIndex):
-        raise TypeError(u'Expected df to have index of DatetimeIndex type, got {} instead'.format(type(df.index)))
+        raise TypeError(u'Expected df to have index of DatetimeIndex type, got {} instead'
+                        .format(type(df.index)))
     freq = freq or df.index.freq or 'D'
-    next_n_periods_index = pd.bdate_range(start=df.index.max(), periods=n_periods + 1, freq=freq)[-n_periods:]
+    next_n_periods_index = pd.bdate_range(start=df.index.max(),
+                                          periods=n_periods + 1, freq=freq)[-n_periods:]
     if isinstance(df, pd.DataFrame):
         df_to_append = pd.DataFrame(columns=df.columns, index=next_n_periods_index)
     elif isinstance(df, pd.Series):
